bf8494610be9b609c7ee9682195862b0b174e9a1
Ophélie Gaudin <ophelie.gaudin@outlook.fr>
2023-06-16 21:54:57 +0200

    contact form success

--- src/routes/Atoms/ContactSection.svelte
+++ src/routes/Atoms/ContactSection.svelte
@@ -5,73 +5,91 @@
 	let phone = '';
 	let subject = '';
 	let message = '';
+	let success = false;
+	let loading = false;
 
 	const createContact = async () => {
+		loading = true;
+
 		try {
 			await supabase
 				.from('contacts')
 				.insert([{ email, sujet: subject, telephone: phone, message }]);
-			// .then((response) => {
-			// 	if (response.status === 201) {
-			// 		const submitInput = document.querySelector('[type = submit]');
-
-			// 		console.log(submitInput);
-
-			// 		// submitInput?.textContent = 'Message envoyé';
-
-			// 		// submitInput?.classList.remove('bg-[var(--color-theme-1)] cursor-pointer');
-			// 		// submitInput?.classList.add('bg-green');
-			// 	}
-			// });
+			success = true;
 		} catch (err) {
 			return err;
 		}
+
+		loading = false;
 	};
 </script>
 
 <div id="contact-us">
 	<div class="section-content w-[80vw]">
 		<h3 class="blue-text text-center my-8">Nous contacter</h3>
-		<form
-			on:submit|preventDefault={createContact}
-			name="contact"
-			id="contact-form"
-			class="dual-div w-full flex-col md:flex-row"
-		>
-			<div class="left-side flex flex-col">
-				<input type="text" required placeholder="Votre email" class="w-full" bind:value={email} />
-				<input
-					type="text"
-					placeholder="Votre numéro de téléphone"
-					class="w-full"
-					bind:value={phone}
-				/>
-				<select name="object" id="object-select" class="w-full" bind:value={subject}>
-					<option value="" disabled selected>Sélectionnez un objet</option>
-					<option value="formations">Informations sur les formations</option>
-					<option value="choregraphies">Informations sur les chorégraphies</option>
-					<option value="other">Autre</option>
-				</select>
+		{#if success}
+			<div class="text-bold p-4 bg-green-400">
+				Nous avons bien reçu votre demande, nous vous répondrons dans les meilleurs délais !
 			</div>
-			<div class="right-side">
-				<textarea
-					required
-					bind:value={message}
-					name="message"
-					id="message-textarea"
-					cols="30"
-					rows="10"
-					placeholder="Votre messsage"
-					class="w-full"
-				/>
+		{:else}
+			<form
+				on:submit|preventDefault={createContact}
+				name="contact"
+				id="contact-form"
+				class="dual-div w-full flex-col md:flex-row"
+			>
+				<div class="left-side flex flex-col">
+					<input
+						type="email"
+						required
+						placeholder="Votre email"
+						class="w-full"
+						disabled={loading}
+						bind:value={email}
+					/>
+					<input
+						type="phone"
+						placeholder="Votre numéro de téléphone"
+						class="w-full"
+						disabled={loading}
+						bind:value={phone}
+					/>
+					<select
+						required
+						name="object"
+						id="object-select"
+						class="w-full"
+						disabled={loading}
+						bind:value={subject}
+					>
+						<option value="" disabled selected>Sélectionnez un objet</option>
+						<option value="formations">Informations sur les formations</option>
+						<option value="choregraphies">Informations sur les chorégraphies</option>
+						<option value="other">Autre</option>
+					</select>
+				</div>
+				<div class="right-side">
+					<textarea
+						required
+						bind:value={message}
+						name="message"
+						id="message-textarea"
+						cols="30"
+						rows="10"
+						placeholder="Votre messsage"
+						disabled={loading}
+						class="w-full"
+					/>
 
-				<input
-					type="submit"
-					value="Envoyer votre message !"
-					class="px-4 bg-[var(--color-theme-1)] text-white cursor-pointer"
-				/>
-			</div>
-		</form>
+					<input
+						type="submit"
+						value="Envoyer votre message !"
+						class="px-4 bg-[var(--color-theme-1)] text-white cursor-pointer"
+						disabled={loading}
+					/>
+				</div>
+			</form>
+		{/if}
 	</div>
 </div>
 
